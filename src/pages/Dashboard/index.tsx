f08260ff75b1ffbbaaccc65946b306e1522fb2bb
Thyago Machado <thymac@gmail.com>
2020-09-25 17:07:51 -0300

    api axios, error handling, styled components props

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,52 +1,71 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
 import { FiChevronRight } from "react-icons/fi";
-import { Title, Form, Repositories } from "./styles";
+import { Title, Form, Repositories, Error } from "./styles";
 
 import logoImg from "../../assets/logo.svg";
 
+import api from "../../services/api";
+import Repository from "../Repository";
+
+interface Repository {
+  id: number;
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [inputError, setInputError] = useState("");
+  const [searchRepo, setSearchRepo] = useState("");
+  const [respositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!searchRepo) {
+      setInputError("Digite o autor/nome do repositório");
+      return;
+    }
+    try {
+      const { data: repository } = await api.get<Repository>(`repos/${searchRepo}`);
+
+      const filteredRepos = respositories.filter((r) => r.id !== repository.id);
+
+      setRepositories([...filteredRepos, repository]);
+      setSearchRepo("");
+      setInputError("");
+    } catch (err) {
+      setInputError("Erro na busca por esse repositório");
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Dashboard</Title>
-      <Form action="">
-        <input type="text" placeholder="Digite o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={(e) => handleAddRepository(e)}>
+        <input
+          value={searchRepo}
+          onChange={(e) => setSearchRepo(e.target.value)}
+          type="text"
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
+      {inputError && <Error>{inputError}</Error>}
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars3.githubusercontent.com/u/7282042?s=460&u=c358d49a0f13148754ffa4c0438d57165b2e38d5&v=4"
-            alt="Thyago"
-          />
-          <div>
-            <strong>My Repository</strong>
-            <p>description kjljkll</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars3.githubusercontent.com/u/7282042?s=460&u=c358d49a0f13148754ffa4c0438d57165b2e38d5&v=4"
-            alt="Thyago"
-          />
-          <div>
-            <strong>My Repository</strong>
-            <p>description kjljkll</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars3.githubusercontent.com/u/7282042?s=460&u=c358d49a0f13148754ffa4c0438d57165b2e38d5&v=4"
-            alt="Thyago"
-          />
-          <div>
-            <strong>My Repository</strong>
-            <p>description kjljkll</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {respositories.map((repository) => (
+          <a key={repository.id}>
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,15 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { shade } from "polished";
+
+interface FormProps {
+  hasError: boolean;
+}
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+`;
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -9,7 +19,7 @@ export const Title = styled.h1`
   line-height: 56px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -21,6 +31,14 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${(props) =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -77,7 +95,8 @@ export const Repositories = styled.div`
     }
 
     div {
-      margin-left: 16px;
+      flex: 1;
+      margin: 16px;
 
       strong {
         font-size: 20px;
